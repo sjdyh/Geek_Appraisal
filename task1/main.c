25f5315bbe08cedc1bba50b129a6b8abdfb4ea9a
SJDYH <3274125417@qq.com>
2022-10-24 13:05:56 +0800

    sjdyh answer

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+#include<stdio.h>
+int main()
+{
+	double a, h = 0.001;
+	printf("please enter the initial value a= ");
+	scanf("%lf", &a);
+	double x = 0, y1 = a, dy_dx = -a, y2 = a - 1, y = a;
+	for (int i = 0; y1 - y2 > 0.001 * h; i++)
+	{
+		y1 = y;
+		dy_dx = -y1;
+		x = x + h;
+		y = y + h * (dy_dx);
+		y2 = y;
+		printf("当x=%lf      yn=%lf\n", x - h, y - h * (dy_dx));
+	}
+	printf("最终定态为%f", y);
 	return 0;
 }
 
